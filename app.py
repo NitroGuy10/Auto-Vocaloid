93b9f5fa89a35ead0875d9f5bd37e229752e0b9a
NitroGuy10 <naviyer10@gmail.com>
2023-08-13 20:43:02 -0600

    Word splitting

--- app.py
+++ app.py
@@ -10,13 +10,19 @@
 #   I guess I can use random numbers to determine the note for syllable
 #   They should be within a certain range,
 #       with a comfortably large jump between each one
+#   I can make the fine pitch be random too so the words sound less sing-songy
 #   Complicated syllables shoud have longer notes
 #   A word's syllables should be back-to-back
 #   There should be a short pause between words
+#   There should be a longer pause between sentences, denoted by ./?/!
+#   Maybe questions can end off with a higher note
+#   You can type spaces into vocaloid to input multiple words at once
 # I can read the screen and use PyAutoGUI's Locate functions
 #   to determine where to click and stuff
 #   https://pyautogui.readthedocs.io/en/latest/screenshot.html#the-locate-functions
-# Each note can have its "pitch" and "expression" parameters set to the max
+# Each note can have its "pitch" parameters set to the max
+#       These are set by clicking and dragging the dots
+#       OR by using the inspector side panel
 # Have the program CTRL+N before inputting every phrase
 # Have "breaths" enabled
 # Be sure to pick a comfortable tempo
@@ -28,6 +34,28 @@
 #
 
 
+from nltk.tokenize import SyllableTokenizer
+from nltk import word_tokenize
+
+SSP = SyllableTokenizer()
+
 while True:
     phrase = input("> ")
-    print(phrase)
+
+    # Tokenize the phrase into words
+    words = word_tokenize(phrase)
+    fixed_words = []
+    # Smoosh contractions together
+    for i in range(len(words)):
+        word = words[i].lower()
+        if "'" in word and i > 0 and fixed_words[-1] not in ".,?!":
+            fixed_words[-1] += word
+        else:
+            fixed_words.append(word)
+    print(fixed_words)
+
+    # Tokenize the words into syllables
+    # syllables = [SSP.tokenize(word) for word in fixed_words]
+    # print(syllables)
+
+    print(" ".join(fixed_words))
